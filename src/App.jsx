233de2223393c6f98c762e9eda5ca5054e2b4d40
Mljungho <mathias.ljungholm@gmail.com>
2021-12-08 16:12:19 +0100

    Configured cypress.json - ChromeWebSecurity & added axios. Added fixture for menu. Added cypress E2E test, visit application & make network call. Added basic UI for App.jsx getting data (Menu) from fixture. Connected app to DB using axios.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import Products from "./modules/Products";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    Products.index().then((data) => {
+      setProducts(data);
+    });
+  }, []);
+
+  const addToOrder = (id) => {
+    debugger;
+  };
+
+  const productsList = products.map((product) => {
+    return (
+      <div key={product.id}>
+        {product.name} - {`${product.price} kr`}{" "}
+        <button onClick={() => addToOrder(product.id)}>Add to Order</button>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <h1>Slowfood</h1>
+      <div data-cy="products-list">{productsList}</div>
+    </>
+  );
+};
+
+export default App;
